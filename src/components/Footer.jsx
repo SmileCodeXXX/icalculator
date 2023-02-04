acb5c26dac4eddec032f5c9bca1aaf35019faac3
Solomon Cammue <s.cammue@gmail.com>
2023-02-04 17:21:35 -0600

    fixed a ui bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Copyright 2023. All Reserve.</p>
+      <p>Copyright 2023. Alright Reserver.</p>
       <p>Made by Smile</p>
       </div>
   )
